558793d41df0baded12d90ccff8d90920757129d
Yuvashree k <95246395+Yuvashree16@users.noreply.github.com>
2025-10-15 15:06:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleProjects() {
+    var projects = document.getElementById('projects');
+    if (projects.style.display === 'none') {
+        projects.style.display = 'block';
+    } else {
+        projects.style.display = 'none';
+    }
+}
